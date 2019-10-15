8d98eee5d46ba085f7bb6842f748c4f65069281c
Leandro Long <leandro.long@gmail.com>
2019-10-15 09:31:09 -0300

    Primeiro dia do Curso AspNet Core MVC

---
+++ Blog/BdScripts/SQLQuery1.sql
@@ -0,0 +1,2 @@
+﻿select * from Posts;
+--insert into Posts (Titulo, Resumo, Categoria) values ('Vingadores Guerra Infinita', 'O filme da Marvel', 'Aventura');
---
+++ Blog/Controllers/PostController.cs
@@ -0,0 +1,75 @@
+﻿using Blog.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Blog.Controllers
+{
+    public class PostController : Controller
+    {
+        private List<Post> lista;
+
+        public PostController()
+        {
+
+            lista = new List<Post>
+            {
+                new Post
+            {
+                Titulo = "Vingadores Guerra Infinita",
+                Categoria = "Aventura",
+                Resumo = "O filme da Marvel ......"
+            },
+                new Post
+            {
+                Titulo = "Shaft",
+                Categoria = "Ação",
+                Resumo = "O filme Shaft ......"
+            },
+                new Post
+            {
+                Titulo = "A Era do Gelo 4",
+                Categoria = "Animação",
+                Resumo = "O filme conta a história ......"
+            },
+                new Post
+            {
+                Titulo = "Star Wars The Rise of Skywalker",
+                Categoria = "Ficção",
+                Resumo = "O filme Star Wars ......"
+            },
+                new Post
+            {
+                Titulo = "Chorona",
+                Categoria = "Terror",
+                Resumo = "O filme ......"
+            }
+        };
+          
+        }
+
+     
+        public IActionResult Index()
+        {
+           // ViewBag.ListaPost = posts;
+
+            return View(lista);
+        }
+
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Adiciona(Post post)
+        {
+           
+            lista.Add(post);
+
+            return View("Index", lista);
+        }
+    }
+}
