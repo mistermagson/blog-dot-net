de14319d4d32308616382e5dc02903cd8c1caf2d
Leandro Long <leandro.long@gmail.com>
2019-10-15 11:53:44 -0300

    Configuração de acesso a Banco usando Entity Framework

--- Blog/Dao/PostDAO.cs
+++ Blog/Dao/PostDAO.cs
@@ -13,42 +13,20 @@ namespace Blog.Dao
     {
         public List<Post> Lista()
         {
-            using (SqlConnection connection = ConnectionFactory.CriaConexaoAberta())
+            using(BlogContext context = new BlogContext())
             {
-                SqlCommand comando = connection.CreateCommand();
-                comando.CommandText = "select * from Posts";
-                IDataReader leitor = comando.ExecuteReader();
-
-                List<Post> posts = new List<Post>();
-
-                while (leitor.Read())
-                {
-                    posts.Add(new Post
-                    {
-                        Id = Convert.ToInt32(leitor["id"]),
-                        Titulo = leitor["titulo"].ToString(),
-                        Resumo = leitor["resumo"].ToString(),
-                        Categoria = leitor["categoria"].ToString()
-                    });
-                }
-                return posts;
+                var lista = context.Posts.ToList();
+                return lista;
             }
+             
         }
 
         public void Adiciona(Post p)
         {
-            using (SqlConnection connection = ConnectionFactory.CriaConexaoAberta())
+            using (BlogContext context = new BlogContext())
             {
-                SqlCommand comando = connection.CreateCommand();
-                comando.CommandText = $"insert into Posts (Titulo, Resumo, Categoria) values (@titulo, @resumo, @categoria)";
-
-                comando.Parameters.Add(new SqlParameter("titulo", p.Titulo));
-                comando.Parameters.Add(new SqlParameter("resumo", p.Resumo));
-                comando.Parameters.Add(new SqlParameter("categoria", p.Categoria));
-
-                comando.ExecuteNonQuery();
-                
-                
+                context.Posts.Add(p);
+                context.SaveChanges();
             }
         }
     }
